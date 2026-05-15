b7f741aee4fd2b00c3eedbe8180c6c114f4b535e
rahul078s <rahulsandhu527@gmail.com>
2026-05-15 19:12:00 +0000

    Create Course and Enrollment model and register them to admin site

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
+from datetime import date
 
 # Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=300)
+    instructor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Enrollment(models.Model):
+    enroll_date = models.DateField(default=date.today)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
